cce21f789b31ce7cde1b768b3431a0ffd8b9975b
Hoang.NV <hoangnv.2510332@usth.edu.vn>
2026-09-24 15:13:59 +0700

    First student commit

---
+++ labwork1/labwork1.py
@@ -0,0 +1,184 @@
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+def isPerfect(n):
+    sumDivs = 0
+    for i in range(1,n):
+        if n%i == 0:
+            sumDivs += i
+    if sumDivs == n:
+        return True
+    return False
+
+def getDivisors(n):
+    divs = []
+    sign = 1
+    n1 = n
+    if n == 0:
+        return "None"
+    if (n1 > 0):
+        n1 += 1
+    else:
+        sign = -1
+        n1 = (-1*n1) + 1
+    for i in range (1, n1):
+        if n%i == 0:
+            divs.append(i*sign)
+    return divs
+
+def remove_dollar_sign(s):
+    s1 = s.split('$')
+    s = ''.join(s1)
+    return s
+
+def toIntList(t):
+    # Expected input [1, 2, 3]; 1, 2, 3; 1 2 3; 123
+    if (type(t) == list):
+        return t
+    t1 = t
+    t1 = t1.split('[')
+    t1 = ''.join(t1)
+    t1 = t1.split(']')
+    t1 = ''.join(t1)
+    if ',' in t:
+        t1 = t1.split(' ')
+        t1 = ''.join(t1)
+        t1 = t1.split(',')
+    elif ' ' in t:
+        t1 = t1.split(' ')
+    t2 = []
+    for i in t1:
+        t2.append(int(i))
+    return t2
+
+def extract_even(t):
+    t1 = []
+    if (type(t) != list):
+        t = toIntList(t)
+    for i in t:
+        if int(i)%2 == 0:
+           t1.append(int(i))
+    return t1
+
+def fact_recursive(n):
+    if n < 0:
+        return -1;
+    # convention: 0! = 1; 1! = 1
+    if n == 0 or n == 1:
+        return 1
+    
+    # n! = n*(n-1)!
+    else:
+        return n*fact(n-1)
+
+def fact(n):
+    temp = 1
+    if n < 0:
+        return -1;
+    for i in range(1,n+1):
+        temp *= i
+    return temp
+
+def getDist(t1, t2):
+    if len(t1) == 1:
+        if t2[0] > t1[0]:
+            return t2[0] - t1[0]
+        return t1[0] - t2[0]
+    if len(t1) == 2:
+        return ((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2)**0.5
+    if len(t1) == 3:
+        return ((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2 + (t2[2] - t1[2])**2)**0.5
+    return -1;
+
+
+#Ex 1
+pi = 3.14
+temp = float(input("Enter Radius: "))
+print(f"Circle Area = {temp*temp*pi}")
+print()
+
+#Ex 2
+temp = float(input("Enter Temperature in Celsius: "))
+print(f"{temp} (C) = {temp*1.8+32} (F)\n")
+
+#Ex 3
+temp = int(input("Enter a Number (Prime Check): "))
+if isPrime(temp):
+    print(f"{temp} is a prime number\n")
+else:
+    print(f"{temp} is NOT a prime number\n")
+
+#Ex 4
+temp = int(input("Enter a Number (Perfect Check): "))
+if isPerfect(temp):
+    print(f"{temp} is a perfect number\n")
+else:
+    print(f"{temp} is NOT a perfect number\n")
+
+#Ex 5
+color = ('Red', 'red', 'Green', 'green', 'Blue', 'blue')
+temp = input("What is your favourite color? ")
+if temp in color:
+    print(f"Your color is at index {color.index(temp) + 1} in my list\n")
+else:
+    print("Sorry, I could not find your color\n")
+
+#Ex 6
+range1 = []
+range2 = []
+range3 = []
+range4 = []
+
+for i in range(0,7):
+    range1.append(i)
+for i in range(1,11,3):
+    range2.append(i)
+for i in range(5,0,-1):
+    range3.append(i)
+for i in range(6,-3,-2):
+    range4.append(i)
+print(f"{range1}\n{range2}\n{range3}\n{range4}\n")
+
+#Ex 7
+temp = input("Enter a string (Remove Dollars): ")
+print(f"{remove_dollar_sign(temp)}\n")
+
+#Ex 8
+temp = input("Enter a list (eg: 2 4 6 -8 -10): ")
+print(f"{extract_even(temp)}\n")
+
+#Ex 9
+temp = int(input("Enter a non-negative integer (Return Factorial): "))
+print(f"Factorial of {temp} is {fact(temp)}\n")
+
+#Ex 10
+temp = int(input("Enter a non-negative integer (Return Divisors): "))
+print(f"Divisors of {temp}: {getDivisors(temp)}\n")
+
+#Ex 11
+temp = int(input("Enter number of axes (maximum 3): "))
+p1 = []
+p2 = []
+ax = ('x', 'y', 'z')
+
+for i in range(0, temp):
+    p1.append(float(input(f"point 1's {ax[i]}: ")))
+    
+for i in range(0, temp):
+    p2.append(float(input(f"point 2's {ax[i]}: ")))
+
+print(f"The distance between point 1 and point 2 is about {round(getDist(p1, p2), 3)}\n")
+
+#Ex 12
+temp = int(input("Enter m (rows): "))
+temp2 = int(input("Enter n (columns): "))
+# rows
+for i in range(0, temp):
+    # message in row i
+    if i == 0 or i == temp-1:
+        print("* "*(temp2-1) + "*")
+    else:
+        print("*" + " "*((temp2-1)*2-1) + "*")
---
+++ labwork1/labwork1b.py
@@ -0,0 +1,70 @@
+sList = []
+cList = []
+mList = []
+sNum = int(input("Enter number of students: "))
+cNum = int(input("Enter number of courses: "))
+
+# Patterns
+def student_pattern_in(i):
+    print()
+    sID = int(input(f"Enter Student {i + 1} ID : "))
+    sName = input(f"Enter Student {i + 1} Name : ")
+    sDoB = input(f"Enter Student {i + 1} Date of Birth: ")
+    return {'ID':sID, 'Name':sName, 'DoB':sDoB}
+
+def course_pattern_in(i):
+    print()
+    cID = int(input(f"Enter Course {i + 1} ID: "))
+    cName = input(f"Enter Course {i + 1} Name: ")
+    return {'ID':cID, 'Name':cName}
+
+def setStudentInfo():
+    for i in range(0,sNum):
+        sList.append(student_pattern_in(i))
+
+def setCourseInfo():
+    for i in range(0,cNum):
+        cList.append(course_pattern_in(i))
+
+def getCourses():
+    print()
+    for i in range(0,cNum):
+        print(f"Course ID: {cList[i].get('ID')}. Course Name: {cList[i].get('Name')}")
+
+def getStudents():
+    print()
+    for i in range(0,sNum):
+        print(f"Student ID: {sList[i].get('ID')}. Student Name: {sList[i].get('Name')}. Date of Birth: {sList[i].get('DoB')}")
+
+def getCourseIndex(n):
+    for i in range (0,cNum):
+        if (cList[i].get('ID') == n):
+            return i
+    return -1
+
+def getStudentIndex(n):
+    for i in range (0,cNum):
+        if (sList[i].get('ID') == n):
+            return i
+    return -1
+
+def setMarks():
+    print("Select a Course")
+    getCourses()
+    print()
+    cID = int(input(f"Enter Course ID: "))
+    for i in range(0,sNum):
+        sMark = float(input(f"Enter Student {sList[i].get('ID')} Mark: "))
+        mList.append({"Course":cList[getCourseIndex(cID)], "Student":sList[i], "Mark":sMark})
+
+def getMarks():
+    print()
+    for i in range(0,sNum):
+        print(f"Course ID: {mList[i].get('Course').get('ID')}. Course Name: {mList[i].get('Course').get('Name')}")
+        print(f"Student ID: {mList[i].get('Student').get('ID')}. Student Name: {mList[i].get('Student').get('Name')}. Mark: {mList[i].get('Mark')}\n")
+
+setStudentInfo()
+setCourseInfo()
+setMarks()
+getMarks()
+
